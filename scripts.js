d518e91dd08b37babb72aec170f507b5583952fd
Harish Kumar Chakravarty <103282023+chakravartyharish@users.noreply.github.com>
2022-09-20 17:58:06 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -314,7 +314,7 @@
         }
 
         // confetti animation
-        function Confetti() {
+       function Confetti() {
           const usedColors = [
             "#FF6633",
             "#FFB399",
@@ -368,93 +368,40 @@
             "#6666FF",
           ]
           var canvas = document.createElement("canvas")
+
+          const ctx = canvas.getContext("2d")
+
           var container = document.querySelector(".canvasRel")
           container.appendChild(canvas)
 
+          const maxNumber = 200
+          const arrConfetti = []
           let W = window.innerWidth
           let H = window.innerHeight
 
-          const context = canvas.getContext("2d")
-          const maxConfettis = 100
-          const particles = []
-
-          const possibleColors = [
-            "DodgerBlue",
-            "OliveDrab",
-            "Gold",
-            "Pink",
-            "SlateBlue",
-            "LightBlue",
-            "Gold",
-            "Violet",
-            "PaleGreen",
-            "SteelBlue",
-            "SandyBrown",
-            "Chocolate",
-            "Crimson",
-          ]
-
-          function randomFromTo(from, to) {
+          function confettiSize(from, to) {
             return Math.floor(Math.random() * (to - from + 1) + from)
           }
 
           function confettiParticle() {
             this.x = Math.random() * W // x
             this.y = Math.random() * H - H // y
-            this.r = randomFromTo(11, 33) // radius
-            this.d = Math.random() * maxConfettis + 11
+            this.r = confettiSize(7, 7) // radius
+            this.d = Math.random() * maxNumber + 11
             this.color =
-              possibleColors[Math.floor(Math.random() * possibleColors.length)]
+              usedColors[Math.floor(Math.random() * usedColors.length)]
             this.tilt = Math.floor(Math.random() * 33) - 11
             this.tiltAngleIncremental = Math.random() * 0.07 + 0.05
-            this.tiltAngle = 0
+            this.tiltAngle = 0.0
 
-            this.draw = function () {
-              context.beginPath()
-              context.lineWidth = this.r / 2
-              context.strokeStyle = this.color
-              context.moveTo(this.x + this.tilt + this.r / 3, this.y)
-              context.lineTo(
-                this.x + this.tilt,
-                this.y + this.tilt + this.r / 5
-              )
-              return context.stroke()
+            this.design = function () {
+              ctx.beginPath()
+              ctx.lineWidth = this.r
+              ctx.strokeStyle = this.color
+              ctx.moveTo(this.x + this.tilt + this.r, this.y)
+              ctx.lineTo(this.x + this.tilt, this.y + this.tilt + this.r)
+              return ctx.stroke()
             }
-          }
-
-          function Draw() {
-            const results = []
-
-            // Magical recursive functional love
-            requestAnimationFrame(Draw)
-
-            context.clearRect(0, 0, W, window.innerHeight)
-
-            for (var i = 0; i < maxConfettis; i++) {
-              results.push(particles[i].draw())
-            }
-
-            let particle = {}
-            let remainingFlakes = 0
-            for (var i = 0; i < maxConfettis; i++) {
-              particle = particles[i]
-
-              particle.tiltAngle += particle.tiltAngleIncremental
-              particle.y += (Math.cos(particle.d) + 3 + particle.r / 2) / 2
-              particle.tilt = Math.sin(particle.tiltAngle - i / 3) * 15
-
-              if (particle.y <= H) remainingFlakes++
-
-              // If a confetti has fluttered out of view,
-              // bring it back to above the viewport and let if re-fall.
-              if (particle.x > W + 30 || particle.x < -30 || particle.y > H) {
-                particle.x = Math.random() * W
-                particle.y = -30
-                particle.tilt = Math.floor(Math.random() * 10) - 20
-              }
-            }
-
-            return results
           }
 
           window.addEventListener(
@@ -468,16 +415,41 @@
             false
           )
 
-          // Push new confetti objects to `particles[]`
-          for (var i = 0; i < maxConfettis; i++) {
-            particles.push(new confettiParticle())
+          function design() {
+            const results = []
+            requestAnimationFrame(design)
+            ctx.clearRect(1.0, 1.0, window.innerWidth, window.innerHeight)
+
+            for (let i = 0; i < maxNumber; i++) {
+              results.push(arrConfetti[i].design())
+            }
+
+            let particle = {}
+            let remainingFlakes = 0
+            for (let i = 0; i < maxNumber; i++) {
+              particle = arrConfetti[i]
+              particle.tiltAngle += particle.tiltAngleIncremental
+              particle.y += (Math.cos(particle.d) + 3 + particle.r / 2) / 2
+              particle.tilt = Math.sin(particle.tiltAngle - i / 3) * 15
+              if (particle.y <= H) remainingFlakes++
+              if (particle.x > W + 30 || particle.x < -30 || particle.y > H) {
+                particle.x = Math.random() * W
+                particle.y = -10
+                particle.tilt = Math.floor(Math.random() * 10) - 50
+              }
+            }
+            return results
           }
 
-          // Initialize
+          for (let i = 0; i < maxNumber; i++) {
+            arrConfetti.push(new confettiParticle())
+          }
+
           canvas.width = W
           canvas.height = H
-          Draw()
+          design()
         }
+
 
         // here is the code for checking the row of the clicked cell and comparing it the isClicked array to see if the row is already clicked or not
         // and generating the confetti animation if the row is complete
